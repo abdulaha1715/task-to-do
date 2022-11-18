46ce3dfbf517c78082ec272660ec83421bf1e48e
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-11-18 23:14:36 +0600

    header component

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>Task Tacker</h1>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    props: {
+      title: {
+        type: String,
+        default: 'Tacker'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
